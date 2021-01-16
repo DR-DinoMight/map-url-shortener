150e3c1b653112f48795e318cdb045fa4ce0eb09
Matt Deloughry <matt@deloughry.co.uk>
2021-01-16 10:33:45 +0000

    Added PWA

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,23 @@
+import { useEffect } from 'react';
 import '../styles/global.css';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
+
+    useEffect(() => {
+        if("serviceWorker" in navigator) {
+            window.addEventListener("load", function () {
+             navigator.serviceWorker.register("/sw.js").then(
+                function (registration) {
+                  console.log("Service Worker registration successful with scope: ", registration.scope);
+                },
+                function (err) {
+                  console.log("Service Worker registration failed: ", err);
+                }
+              );
+            });
+          }
+    }, [])
+
     return <Component {...pageProps} />;
 }
